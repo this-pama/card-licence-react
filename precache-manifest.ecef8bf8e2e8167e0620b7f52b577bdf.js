dce325d1eea5d0f3205a3d2d8daf8dbaff007e0d
Dapo Aderemi <adedapopaul@yahoo.com>
2019-06-09 22:02:19 -0700

    Updates

---
+++ precache-manifest.ecef8bf8e2e8167e0620b7f52b577bdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d578aee042826e8b111350415f4b5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1400332ce2103b0ce6b9",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ff92aeb6e6659e999f2d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1400332ce2103b0ce6b9",
+    "url": "/static/js/2.a3cb9279.chunk.js"
+  },
+  {
+    "revision": "ff92aeb6e6659e999f2d",
+    "url": "/static/js/main.990c1570.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
