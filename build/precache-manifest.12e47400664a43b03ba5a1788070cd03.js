668d571e648813e8f3a62c702598e23af0802ca4
Dapo Aderemi <adedapopaul@yahoo.com>
2019-06-12 11:55:32 -0700

    update gh-pages

---
+++ build/precache-manifest.12e47400664a43b03ba5a1788070cd03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6efe68e65748896636e242c7b66153",
+    "url": "/card-licence-react/index.html"
+  },
+  {
+    "revision": "1400332ce2103b0ce6b9",
+    "url": "/card-licence-react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "3eb68c4da937fb2240a6",
+    "url": "/card-licence-react/static/css/main.986bf13c.chunk.css"
+  },
+  {
+    "revision": "1400332ce2103b0ce6b9",
+    "url": "/card-licence-react/static/js/2.a3cb9279.chunk.js"
+  },
+  {
+    "revision": "3eb68c4da937fb2240a6",
+    "url": "/card-licence-react/static/js/main.c47e68b3.chunk.js"
+  },
+  {
+    "revision": "94194c38ceecd650cdfb",
+    "url": "/card-licence-react/static/js/runtime~main.6aa7acf2.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/card-licence-react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
